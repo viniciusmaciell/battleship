eeb8fc442860d4b48e4107f88d3a5a949d41c6f9
devcamila <camilacerda3@gmail.com>
2021-12-29 20:22:46 -0300

    Atualizando método posicionar navios do computador.

--- src/br/com/letscode/projetojava/NaviosComputador.java
+++ src/br/com/letscode/projetojava/NaviosComputador.java
@@ -3,31 +3,36 @@ package br.com.letscode.projetojava;
 import java.util.Random;
 
 public class NaviosComputador {
-    public static void main(String[] args) {
-        int[] coordenadasPc = new int[2];
+    final int QUANTIDADE_NAVIOS = 10;
+    String[][] naviosPosicionados = new String[QUANTIDADE_NAVIOS][QUANTIDADE_NAVIOS];
 
+    public void inicializarGrelha() {
+        final int DIMENSAO_TABULEIRO = 10;
+        for (int i = 0; i < DIMENSAO_TABULEIRO; i++) {
+            for (int j = 0; j < DIMENSAO_TABULEIRO; j++) {
+                this.naviosPosicionados[i][j] = " ";
+            }
+        }
+    }
+
+    public void posicionarNaviosPc() {
+        int[] coordenadasPc = new int[2];
         Random aleatorio = new Random();
-        Integer[][] naviosPosicionados = new Integer[10][10];
         int naviosPc = 0;
         int linha, coluna;
 
-        while (naviosPc < 10) {
+        while (naviosPc < 3) {
             coordenadasPc[0] = aleatorio.nextInt(10);
             coordenadasPc[1] = aleatorio.nextInt(10);
 
             linha = coordenadasPc[0];
             coluna = coordenadasPc[1];
 
-            if (naviosPosicionados[linha][coluna] == null) {
-                naviosPosicionados[linha][coluna] = 1;
+            if (naviosPosicionados[linha][coluna] == " ") {
+                naviosPosicionados[linha][coluna] = String.valueOf("N");
                 naviosPc++;
             }
-        }
-
-        for (int i = 0; i < naviosPosicionados.length; i++) {
-            for (int j = 0; j < naviosPosicionados.length; j++) {
-                System.out.println(naviosPosicionados[i][j]);
-            }
+            Grelha.atualizarGrelha(naviosPosicionados);
         }
     }
 }

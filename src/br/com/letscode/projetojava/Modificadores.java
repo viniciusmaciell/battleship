14536f3b97b51ae3bc3dddbe28a25419625e471c
devcamila <camilacerda3@gmail.com>
2022-01-11 21:39:11 -0300

    Adição de cores na Grelha.

--- src/br/com/letscode/projetojava/Modificadores.java
+++ src/br/com/letscode/projetojava/Modificadores.java
@@ -2,12 +2,19 @@ package br.com.letscode.projetojava;
 
 public enum Modificadores {
 
-    NAVIO("N"),
+//    NAVIO("N"),
+//    AGUA(" "),
+//    TIRO_NA_AGUA("-"),
+//    TIRO_NO_NAVIO("*"),
+//    TIRO_NO_NAVIO_NAVIO("X"),
+//    TIRO_NA_AGUA_NAVIO("n");
+
+    NAVIO(Colors.YELLOW_BRIGHT + "N" + Colors.RESET),
     AGUA(" "),
-    TIRO_NA_AGUA("-"),
-    TIRO_NO_NAVIO("*"),
-    TIRO_NO_NAVIO_NAVIO("X"),
-    TIRO_NA_AGUA_NAVIO("n");
+    TIRO_NA_AGUA(Colors.BLUE + "-" + Colors.RESET),
+    TIRO_NO_NAVIO(Colors.RED + "*" + Colors.RESET),
+    TIRO_NO_NAVIO_NAVIO(Colors.RED_BRIGHT + "X" + Colors.RESET),
+    TIRO_NA_AGUA_NAVIO(Colors.YELLOW + "n" + Colors.RESET);
 
     private final String nome;
 

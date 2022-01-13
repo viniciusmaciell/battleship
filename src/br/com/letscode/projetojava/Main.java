eb7fb78b5f510fe5ec6831d7074b7b3028f014d3
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-01-13 19:40:29 -0300

    Cria instância de Game.

--- src/br/com/letscode/projetojava/Main.java
+++ src/br/com/letscode/projetojava/Main.java
@@ -3,6 +3,7 @@ package br.com.letscode.projetojava;
 
 public class Main {
     public static void main(String[] args) {
-        Game.executarGame();
+        Game jogo = new Game();
+        jogo.executarGame();
     }
 }
